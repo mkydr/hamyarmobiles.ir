718127c1d95511eadc05b3d7f590d17b3195ea4e
Arian Ebrahimi <ae@retal.tech>
2023-04-09 00:52:42 +0330

    Page routing and implementing

--- hamyarmobiles/src/app/app-routing.module.ts
+++ hamyarmobiles/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { ServicesComponent } from './services/services.component'
+import { AboutComponent } from './about/about.component'
+import { BlogComponent } from './blog/blog.component'
+import { ContactComponent } from './contact/contact.component'
+import { ShopComponent } from './shop/shop.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path:"home", component: HomeComponent},
+  // {path:"portfolio", component: PortfolioComponent, children: [
+  //     {path: "first", component: PortfolioDetailComponent}
+  //   ]},
+  // {path: "faq", component: FaqComponent},
+  {path: "services", component: ServicesComponent},
+  {path: "about", component: AboutComponent},
+  {path: "blog", component: BlogComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "shop", component: ShopComponent},
+  // {path: "**", component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hamyarmobiles/src/app/app.module.ts
+++ hamyarmobiles/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    MenuComponent,
+    ServicesComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
